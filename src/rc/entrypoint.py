78be64d4e4cc68488e6fbba5c2faf3be18760f90
paul.frick <paul.frick@tu-braunschweig.de>
2025-05-24 01:24:39 +0200

    added screen timer

--- src/rc/entrypoint.py
+++ src/rc/entrypoint.py
@@ -1,7 +1,7 @@
 from tools.network import network_init
 from config.config import HEADLESS_MODE
 import time
-
+import datetime
 
 def init():
     if not HEADLESS_MODE:
@@ -16,6 +16,7 @@ def _init_standard():
     from rc.gui import GUI
     gui = GUI()
     gui.display_image('assets/connecting_screen.png')
+    now = datetime.datetime.now()
     try:
         network_init()
     except Exception as e:
@@ -23,3 +24,6 @@ def _init_standard():
         time.sleep(5)
         raise e
 
+    # display the screen at least 5 seconds
+    while datetime.datetime.now() - now < datetime.timedelta(seconds=5):
+        time.sleep(0.3)
