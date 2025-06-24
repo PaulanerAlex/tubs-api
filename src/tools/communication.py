2224f96fdb25cfc1d4c5590e9f2b50bb4baacc5d
paul.frick <paul.frick@tu-braunschweig.de>
2025-06-24 16:19:30 +0200

    removed debug print, added log

--- src/tools/communication.py
+++ src/tools/communication.py
@@ -178,7 +178,8 @@ class Communication:
         '''
         Callback function for the subscriber. Parses the 'command' message and passes the arguments to the simulation pipe.
         '''
-        print(f'communication message received: {sample.payload.to_string()}')
+        if DEBUG_MODE:
+            self.log.debug_plain(f'[RECEIVED]{sample.payload.to_string()}')
         msg = sample.payload.to_string()
         (
         head,
